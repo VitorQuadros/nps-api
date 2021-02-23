857aca87cd2124bbd4544a83edfc708ffee3a715
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-23 18:06:16 -0300

    feat (user controller): email verification

--- src/database/controllers/UserController.ts
+++ src/database/controllers/UserController.ts
@@ -1,10 +1,29 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../../models/User';
 
 class UserController {
   async create(req: Request, res: Response) {
-    const { body } = req;
-    console.log(body);
-    return res.send();
+    const { name, email } = req.body;
+    const usersRepository = getRepository(User);
+
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
+
+    if (userAlreadyExists) {
+      return res.status(400).json({
+        error: 'User already exists.',
+      });
+    }
+
+    const user = usersRepository.create({
+      name, email,
+    });
+
+    await usersRepository.save(user);
+
+    return res.json(user);
   }
 }
 
