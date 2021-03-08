1c2a896ce25f74811320615ca1178afc966f42ee
Vitor Quadros <viitorquadros@hotmail.com>
2021-03-08 19:38:45 -0300

    feat (nps controller): calculating nps

---
+++ src/controllers/NpsController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+
+class NpsController {
+  /*
+   Detratores = 0 - 6
+   Passivos = 7 - 8
+   Promotores = 9 - 10
+
+   (Numero de promotores - Numero de detratores) / (numero de respondentes) x 100
+   */
+
+  async execute(request: Request, response: Response) {
+    const { survey_id } = request.params;
+
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    const surveysUsers = await surveysUsersRepository.find({
+      survey_id,
+    });
+
+    const detractor = surveysUsers.filter((survey) => survey.value >= 0 && survey.value <= 6)
+      .length;
+    const promoter = surveysUsers.filter((survey) => survey.value >= 9 && survey.value <= 10)
+      .length;
+    const passive = surveysUsers.filter((survey) => survey.value >= 7 && survey.value <= 8)
+      .length;
+    const totalAnswers = surveysUsers.length;
+
+    const calculate = ((promoter - detractor) / totalAnswers) * 100;
+
+    return response.json({
+      detractor, promoter, passive, totalAnswers, nps: calculate,
+    });
+  }
+}
+
+export { NpsController };
